2ef88861c879e24d45cc5e8cb6ab975f7b541638
Calvin Spealman <calvin@pantechnoco.com>
2010-11-22 08:59:52 -0500

    Move bash_profile to bashrc, because profile is only loaded on login shells.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-ln -s $(pwd)/bash_profile ~/.bash_profile
+ln -s $(pwd)/bashrc ~/.bashrc
 ln -s $(pwd)/screenrc ~/.screenrc
 ln -s $(pwd)/vim ~/.vim
 ln -s $(pwd)/vimrc ~/.vimrc
