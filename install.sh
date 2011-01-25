5f56b0e8027c0e00461ef91d15465f42b7a0ea51
Calvin Spealman <ironfroggy@gmail.com>
2011-01-25 09:09:08 -0500

    Add bash_profile

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ln -s $(pwd)/bashrc ~/.bashrc
+ln -s $(pwd)/bash_profile ~/.bash_profile
 ln -s $(pwd)/screenrc ~/.screenrc
 ln -s $(pwd)/vim ~/.vim
 ln -s $(pwd)/vimrc ~/.vimrc
